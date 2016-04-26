a3440d7ee5147c7cea2cfe0dccdca5c37b9cd8e5
EvilKhaosKat <evilkhaoskat@gmail.com>
2016-04-26 21:44:14 +0400

    Book class implementation

---
+++ Book.py
@@ -0,0 +1,18 @@
+class Book:
+    title = "No title"
+    cost = 0
+
+    def __init__(self, title, cost):
+        self.title = title
+        self.cost = cost
+
+    def __str__(self, *args, **kwargs):
+        return self.__repr__()
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__  # TODO check strictly on title/cost?
+
+    def __repr__(self):
+        return "Book(title='{}', cost={})".format(self.title, self.cost)
+
+
--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import json
 
+from Book import Book
+
 
 def get_books_json():
     with open("books.json") as f:
         return f.read()
 
 
+def create_books_list(books_decoded):
+    books_list = []
+
+    for book_dict in books_decoded:
+        books_list.append(Book(**book_dict))
+
+    return books_list
+
+
 books_json = get_books_json()
+
+
 print("books_json:%s" % books_json)
 
 
-books_decoded = json.loads(books_json)
+books_json_decoded = json.loads(books_json)  # raw python objects: list of dicts
+books_list = create_books_list(books_json_decoded)
+
+print("books_list:%s" % books_list)
